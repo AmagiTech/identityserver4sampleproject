a03f68602bf441abae01c97a64a39e2e55dec523
Muharrem Barkın <barkinmuharrem@outlook.com>
2020-02-19 16:47:10 +0300

    First Commit

---
+++ BankOfDotNet/BankOfDotNet.ConsoleClient/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace BankOfDotNet.ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+
+        private static async Task MainAsync()
+        {
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(
+                new ClientCredentialsTokenRequest
+                {
+                    Address = disco.TokenEndpoint,
+                    ClientId = "client",
+                    ClientSecret = "secret",
+                    Scope = "bankOfDotNetApi"
+                });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var customerInfo = new StringContent(
+                JsonConvert.SerializeObject(
+                    new { Id = 10, FirstName = "Muharrem", LastName = "Barkın" }),
+                Encoding.UTF8, "application/json"
+                );
+            var createCustomerResponse = await client.PostAsync("http://localhost:55153/api/customers",
+                customerInfo);
+            if (!createCustomerResponse.IsSuccessStatusCode)
+            {
+                Console.WriteLine(createCustomerResponse.StatusCode);
+            }
+
+            var getCustomerResponse = await client.GetAsync("http://localhost:55153/api/customers");
+            if (!getCustomerResponse.IsSuccessStatusCode)
+            {
+                Console.WriteLine(createCustomerResponse.StatusCode);
+            }
+            else
+            {
+                var content = await getCustomerResponse.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
