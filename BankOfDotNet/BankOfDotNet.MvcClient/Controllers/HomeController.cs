01890f068f9dbe085d666251a8a4cc104bef7d53
Muharrem Barkın <barkinmuharrem@outlook.com>
2020-02-20 12:06:48 +0300

    Update HomeController.cs

--- BankOfDotNet/BankOfDotNet.MvcClient/Controllers/HomeController.cs
+++ BankOfDotNet/BankOfDotNet.MvcClient/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using BankOfDotNet.MvcClient.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
 
 namespace BankOfDotNet.MvcClient.Controllers
 {
@@ -36,6 +37,12 @@ namespace BankOfDotNet.MvcClient.Controllers
             return View();
         }
 
+
+        public async Task Logout()
+        {
+            await HttpContext.SignOutAsync("Cookies");
+            await HttpContext.SignOutAsync("oidc");
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
